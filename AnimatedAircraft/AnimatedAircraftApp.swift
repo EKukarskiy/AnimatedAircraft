58eaa1daf5dced655ddf7453672dfedddf94aac8
Evgeniy Kukarskiy <ekukarskiy@gmail.com>
2023-12-19 19:11:41 +0500

    Initial Commit

---
+++ AnimatedAircraft/AnimatedAircraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedAircraftApp.swift
+//  AnimatedAircraft
+//
+//  Created by Evgeniy K on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedAircraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
